04bf7de12e118898121a1321628386773e40d6f7
Snah79 <cendrowski@hotmail.com>
2025-08-14 13:09:36 +0200

    Disable client side debugging

--- PersistentEmpiresClient/PersistentEmpiresClient/PersistentEmpiresClient.cs
+++ PersistentEmpiresClient/PersistentEmpiresClient/PersistentEmpiresClient.cs
@@ -14,15 +14,16 @@ namespace PersistentEmpiresClient
         protected override void InitializeGameStarter(Game game, IGameStarter starterObject)
         {
             base.InitializeGameStarter(game, starterObject);
+
+            // Disable client side debugging
+            Debug.DebugManager = null;
+
             CompressionBasic.MissionObjectIDCompressionInfo = new CompressionInfo.Integer(-1, 1000000, true);
             CompressionBasic.RoundGoldAmountCompressionInfo = new CompressionInfo.Integer(0, Int32.MaxValue, true);
             CompressionBasic.MapTimeLimitCompressionInfo = new CompressionInfo.Integer(1, int.MaxValue, true);
             CompressionBasic.BigRangeLowResLocalPositionCompressionInfo = new CompressionInfo.Float(-50000f, 50000f, 16);
             CompressionBasic.PositionCompressionInfo = new CompressionInfo.Float(-200f, 10385f, 22);
             CompressionBasic.PlayerCompressionInfo = new CompressionInfo.Integer(-1, 1048576, true);
-
-
-
 
             starterObject.AddModel(new PEAgentStatCalculateModel());
             starterObject.AddModel(new DefaultItemValueModel());

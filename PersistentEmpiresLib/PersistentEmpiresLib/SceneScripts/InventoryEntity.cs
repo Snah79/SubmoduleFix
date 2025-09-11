abe0701ee19b2a7f2cd37331f511041bc566702c
Snah79 <cendrowski@hotmail.com>
2025-09-11 14:34:17 +0200

    Revert "Squashed commit of the following:"
    This reverts commit d99c01de363d08c863fb3dc5c1053398e56c0e1f.

--- PersistentEmpiresLib/PersistentEmpiresLib/SceneScripts/InventoryEntity.cs
+++ PersistentEmpiresLib/PersistentEmpiresLib/SceneScripts/InventoryEntity.cs
@@ -79,7 +79,6 @@ namespace PersistentEmpiresLib.SceneScripts
         private static readonly ActionIndexCache act_pickup_from_left_up_horseback_left_end = ActionIndexCache.Create("act_pickup_from_left_up_horseback_left_end");
         protected override bool LockUserFrames { get => false; }
         protected override bool LockUserPositions { get => false; }
-        public static PE_InventoryEntity _current;
 
         private string GenerateId()
         {
@@ -274,10 +273,6 @@ namespace PersistentEmpiresLib.SceneScripts
                 this.OpenInventory(userAgent);
 #endif
             }
-
-#if CLIENT
-            _current = this;
-#endif
         }
 
         public void OnEntityRemove()

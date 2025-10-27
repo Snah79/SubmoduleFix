35b8025ee518d9fc452b03706be5acc4dafaeea5
Michal Cendrowski <cendrowski@hotmail.com>
2025-10-27 20:36:59 +0000

    Drowning Server

--- PersistentEmpiresLib/PersistentEmpiresLib/PersistentEmpiresMission/MissionBehaviors/DrowningBehavior.cs
+++ PersistentEmpiresLib/PersistentEmpiresLib/PersistentEmpiresMission/MissionBehaviors/DrowningBehavior.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using TaleWorlds.Core;
 using TaleWorlds.Engine;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
@@ -31,36 +33,51 @@ namespace PersistentEmpiresLib.PersistentEmpiresMission.MissionBehaviors
             this.LowerLimit = lowerLimit.GetGlobalFrame().origin.Z;
             this.LastCheckedAt = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
         }
-
+        
         public override void OnMissionTick(float dt)
         {
-            if ((this.LastCheckedAt + this.Duration < DateTimeOffset.UtcNow.ToUnixTimeSeconds()) && this.IsSetProperly)
+            if ((LastCheckedAt + this.Duration < DateTimeOffset.UtcNow.ToUnixTimeSeconds()) && this.IsSetProperly)
             {
-                this.LastCheckedAt = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-                foreach (Agent agent in base.Mission.Agents.ToList())
+                var delAgentList = new List<Agent>();
+
+                LastCheckedAt = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+                foreach (Agent agent in base.Mission.Agents)
                 {
-                    if (agent.IsActive() && agent.Position.Z < UpperLimit && agent.Position.Z > LowerLimit)
+                    //if (agent.IsActive() && agent.Position.Z < UpperLimit && agent.Position.Z > LowerLimit)
+                    if (agent.IsActive() && !agent.IsHuman)
                     {
-                        Blow blow = new Blow(agent.Index);
-                        blow.DamageType = TaleWorlds.Core.DamageTypes.Pierce;
-                        blow.BoneIndex = agent.Monster.HeadLookDirectionBoneIndex;
-                        blow.GlobalPosition = agent.Position;
-                        blow.GlobalPosition.z = blow.GlobalPosition.z + agent.GetEyeGlobalHeight();
-                        blow.BaseMagnitude = 40;
-                        blow.WeaponRecord.FillAsMeleeBlow(null, null, -1, -1);
-                        blow.InflictedDamage = 40;
-                        blow.SwingDirection = agent.LookDirection;
-                        MatrixFrame frame = agent.Frame;
-                        blow.SwingDirection = frame.rotation.TransformToParent(new Vec3(-1f, 0f, 0f, -1f));
-                        blow.SwingDirection.Normalize();
-                        blow.Direction = blow.SwingDirection;
-                        blow.DamageCalculated = true;
-                        blow.IsFallDamage = true;
-                        sbyte mainHandItemBoneIndex = agent.Monster.MainHandItemBoneIndex;
-                        AttackCollisionData attackCollisionDataForDebugPurpose = AttackCollisionData.GetAttackCollisionDataForDebugPurpose(false, false, false, true, false, false, false, false, false, false, false, false, CombatCollisionResult.StrikeAgent, -1, 0, 2, blow.BoneIndex, BoneBodyPartType.Head, mainHandItemBoneIndex, Agent.UsageDirection.AttackLeft, -1, CombatHitResultFlags.NormalHit, 0.5f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, Vec3.Up, blow.Direction, blow.GlobalPosition, Vec3.Zero, Vec3.Zero, agent.Velocity, Vec3.Up);
-                        agent.RegisterBlow(blow, attackCollisionDataForDebugPurpose);
+                        delAgentList.Add(agent);                        
                     }
                 }
+
+                delAgentList.ForEach(agent =>
+                {
+                    Mission.Current.KillAgentCheat(agent);
+
+                    //Blow blow = new Blow(agent.Index);
+                    ////blow.DamageType = TaleWorlds.Core.DamageTypes.Pierce;
+                    //blow.DamageType = DamageTypes.Blunt;
+                    //blow.BoneIndex = agent.Monster.HeadLookDirectionBoneIndex;
+                    //blow.GlobalPosition = agent.Position;
+                    //blow.GlobalPosition.z = blow.GlobalPosition.z + agent.GetEyeGlobalHeight();
+                    //blow.BaseMagnitude = 40;
+                    //blow.WeaponRecord.FillAsMeleeBlow(null, null, -1, -1);
+                    //blow.InflictedDamage = 40;
+                    //blow.SwingDirection = agent.LookDirection;
+
+                    //MatrixFrame frame = agent.Frame;
+                    //blow.SwingDirection = frame.rotation.TransformToParent(new Vec3(-1f, 0f, 0f, -1f));
+                    //blow.SwingDirection.Normalize();
+
+                    //blow.Direction = blow.SwingDirection;
+                    //blow.DamageCalculated = true;
+                    //blow.IsFallDamage = true;
+                    //sbyte mainHandItemBoneIndex = agent.Monster.MainHandItemBoneIndex;
+                    //var attackCollisionDataForDebugPurpose = AttackCollisionData.GetAttackCollisionDataForDebugPurpose(false, false, false, true, false, false, false, false, false, false, false, false, CombatCollisionResult.StrikeAgent, -1, 0, 2, blow.BoneIndex, BoneBodyPartType.Head, mainHandItemBoneIndex, Agent.UsageDirection.AttackLeft, -1, CombatHitResultFlags.NormalHit, 0.5f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, Vec3.Up, blow.Direction, blow.GlobalPosition, Vec3.Zero, Vec3.Zero, agent.Velocity, Vec3.Up);
+                    //agent.RegisterBlow(blow, in attackCollisionDataForDebugPurpose);
+                });
+                delAgentList.Clear();
             }
         }
     }

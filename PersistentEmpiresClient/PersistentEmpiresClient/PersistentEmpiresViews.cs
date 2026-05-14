e560e1b46043c19b54e814ee503bf9336c284b40
Snah79 <cendrowski@hotmail.com>
2026-05-14 18:50:18 +0200

    asd

--- PersistentEmpiresClient/PersistentEmpiresClient/PersistentEmpiresViews.cs
+++ PersistentEmpiresClient/PersistentEmpiresClient/PersistentEmpiresViews.cs
@@ -13,7 +13,7 @@ namespace PersistentEmpires.Views
     [ViewCreatorModule]
     public class PersistentEmpireViews
     {
-        [ViewMethod("PERoleplay")]
+        [ViewMethod("PersistentEmpires")]
         public static MissionView[] OpenPersistentEmpires(Mission mission)
         {
             List<MissionView> list = new List<MissionView>();
